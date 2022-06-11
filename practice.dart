e8f213ee4793b0c3afb46c7cc927e4d6cb8fcb67
유찬홍 <chanhongy6@naver.com>
2022-06-11 13:36:37 +0900

    dart 기본문법

--- practice.dart
+++ practice.dart
@@ -1,4 +1,71 @@
 void main() {
-  int a = 10;
+  //변수 선언
+  var name = "유찬홍";
+
+  print(name);
+
+  var name2 = "대마고";
+
+  print(name2);
+
+  //변수 값 바꾸기
+  name = "플러터";
+
+  print(name);
+
+  //정수 integer
+
+  int num1 = 10;
+  print(num1);
+
+  int num2 = 20;
+  print(num1 + num2);
+  print(num1 - num2);
+  print(num1 / num2);
+  print(num1 * num2);
+
+  //실수 double
+
+  double num3 = 2.5;
+  double num4 = 0.5;
+  print(num3 + num4);
+  print(num3 - num4);
+  print(num3 / num4);
+  print(num3 * num4);
+
+  //맞다/틀리다 boolean
+  bool isTrue = true;
+  bool isFalse = false;
+
+  print(isTrue);
+  print(isFalse);
+
+  //글자 타입 String
+  String a = "이정호";
+  String b = "김연우";
+
   print(a);
+  print(b);
+  print(a + ' ' + b); //이정호 김연우 출력
+  print('${a} ${b}'); //이정호 김연우 출력
+
+  //var 타입추론
+  var c = '조병진';
+  var d = 20;
+  print(c.runtimeType); //c가 무슨 타입인지
+  print(d);
+  //한번 선언하면 다른 타입으로 변경 불가능
+
+  //다이나믹=ㄹㅇ 다됨
+  dynamic e = '김성원';
+  print(e);
+  dynamic f = 10;
+  print(f);
+  //한번 선언해도 다른 타입으로 변경 가능 ex) 원래 정수형인데 문자형으로 바꾸기
+
+  //상수 설정
+  final String g = '김철우'; //final과 const로 수정할수 없게 지정할수 있다
+  print(g);
+  final h = 32; //물론 var같이 형식을 생략할수 있다
+  print(h);
 }
