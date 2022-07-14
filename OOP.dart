5236dd52550b34bdb7b4374762043ffe9cc70c1e
Yoochanhong <chanhongy6@naver.com>
2022-07-14 22:00:10 +0900

    객체지향

--- OOP.dart
+++ OOP.dart
@@ -6,9 +6,8 @@ void main(){
   blackPink.sayHello();
   blackPink.introduce();
 }
-
-// Idol class
-// name (이름) - 변수
+// idol class
+// name(이름) - 변수
 // members (멤버들) - 변수
 // sayHello (인사) - 함수
 // introduce (멤버소개) - 함수
@@ -17,10 +16,11 @@ class Idol{
   List<String> members = ['지수', '제니', '리사', '로제'];
 
   void sayHello(){
-    print('안녕하세요 블랙핑크입니다.');
+    print("안녕하세요 저희는 블랙핑크입니다.");
+  }
+  void introduce(){
+    print("저희 멤버는 지수, 제니, 리사, 로제가 있습니다.");
   }
 
-  void introduce(){
-    print('저희 멤버는 지수, 제니, 리사, 로제가 있습니다.');
-  }
+
 }
