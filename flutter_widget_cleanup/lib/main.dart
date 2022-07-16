485023408f0ae55049cce443e4a0f68740f3a524
Yoochanhong <chanhongy6@naver.com>
2022-07-16 15:22:20 +0900

    ?

--- flutter_widget_cleanup/lib/main.dart
+++ flutter_widget_cleanup/lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              '조병진',
             ),
             Text(
               '$_counter',
