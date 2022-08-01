b8111d835f822c91240ee142ed8352fbfb6155b2
Yoochanhong <chanhongy6@naver.com>
2022-08-02 02:10:01 +0900

    doit 플러터 스위치 만들기
    statefulWidget 이용하기

---
+++ doit_switch/android/app/src/main/kotlin/com/example/doit_switch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.doit_switch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ doit_switch/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyApp();
+  }
+}
+
+class _MyApp extends State<MyApp> {
+  var switchValue = false;
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      darkTheme: ThemeData.light(),
+      home: Scaffold(
+        body: Center(
+          child: Switch(
+            value: switchValue,
+            onChanged: (value) {
+              setState(() {
+                print(value);
+                switchValue = value;
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
