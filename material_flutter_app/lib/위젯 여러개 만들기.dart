f9a0ae502f64117dd1b85dcedbc17b24c0bea696
데탑 유찬홍 <chanhongy6@naver.com>
2022-08-20 19:05:53 +0900

    Scaffold를 이용한 Material 디자인 적용

---
+++ material_flutter_app/lib/위젯 여러개 만들기.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MaterialFlutterApp(),
+    );
+  }
+}
+
+class MaterialFlutterApp extends StatefulWidget {
+  const MaterialFlutterApp({Key? key}) : super(key: key);
+
+  @override
+  State<MaterialFlutterApp> createState() => _MaterialFlutterAppState();
+}
+
+class _MaterialFlutterAppState extends State<MaterialFlutterApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton:
+          FloatingActionButton(child: Icon(Icons.add), onPressed: () {}),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              Icon(Icons.android),
+              Text('android'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
