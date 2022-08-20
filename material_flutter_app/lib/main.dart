4370586dbaaa7c29c618e9fac3573ac00272d30c
데탑 유찬홍 <chanhongy6@naver.com>
2022-08-20 19:05:22 +0900

    Scaffhold를 이용한 material 디자인 적용

---
+++ material_flutter_app/lib/main.dart
@@ -0,0 +1,34 @@
+/*import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MaterialFlutterApp(),
+    );
+  }
+}
+
+class MaterialFlutterApp extends StatefulWidget {
+  const MaterialFlutterApp({Key? key}) : super(key: key);
+
+  @override
+  State<MaterialFlutterApp> createState() => _MaterialFlutterAppState();
+}
+
+class _MaterialFlutterAppState extends State<MaterialFlutterApp> {
+  @override
+  Widget build(BuildContext context) {
+    //return null; build 함수는 화면에 표시하는 함수인데 null이 들어가있으면 아무것도 표시할수 없으므로 오류가 생김
+    return Scaffold();
+  }
+}
+*/
