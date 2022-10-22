906dd683c5c8271eb169b4449209f192f38c85d9
Yoochanhong <chanhongy6@naver.com>
2022-10-22 14:26:26 +0900

    간단한 홈 화면 만들기

---
+++ splash_example/lib/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Splash test'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text('메인 화면'),
+          ],
+        ),
+      ),
+    );
+  }
+}
