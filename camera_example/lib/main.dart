6d849552104592732108c8a60841d4a00a5d86e5
Yoochanhong <chanhongy6@naver.com>
2022-10-25 20:09:21 +0900

    변수 공유 문제로 하나로 합치기

--- camera_example/lib/main.dart
+++ camera_example/lib/main.dart
@@ -4,10 +4,9 @@ import 'dart:async';
 import 'package:camera/camera.dart';
 import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart';
-import 'TakePictureScreen.dart';
 
 
-void main() async {
+Future<void> main() async {
   final cameras = await availableCameras();
   final firstCamera = cameras.first;
   runApp(MyApp());
@@ -20,7 +19,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TakePictureScreen(),
+      home: TakePictureScreen(
+      ),
     );
+  }
+}
+
+
+class TakePictureScreen extends StatefulWidget {
+  final CameraDescription camera;
+  const TakePictureScreen(@required this.camera);
+
+  @override
+  State<TakePictureScreen> createState() => _TakePictureScreenState();
+}
+
+class _TakePictureScreenState extends State<TakePictureScreen> {
+  CameraController? controller;
+  Future<void>? initializeControllerFuture;
+
+  @override
+  void initState(){
+    super.initState();
+    controller = CameraController(widget.camera, ResolutionPreset.medium);
+    initializeControllerFuture = controller!.initialize();
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+    controller!.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
