807619b79b78765aaaf241f7dc831592bde2e94d
Yoochanhong <chanhongy6@naver.com>
2022-10-25 21:57:47 +0900

    CameraController를 사용하여 사진 찍기

--- camera_example/lib/main.dart
+++ camera_example/lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async {
   final cameras = await availableCameras();
@@ -57,6 +59,26 @@ class TakePictureScreenState extends State<TakePictureScreen> {
             return CameraPreview(controller!);
           } else {
             return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        onPressed: () async {
+          try {
+            await initializeControllerFuture;
+            final path = join((await getTemporaryDirectory()).path,
+              '${DateTime.now()}.png',
+            );
+
+            await controller.takePicture(path);
+
+            Navigator.push(context, MaterialPageRoute(
+              builder: (context) => DisplayPictureScreen(),
+            ),
+            );
+          } catch (e){
+            print(e);
           }
         },
       ),
