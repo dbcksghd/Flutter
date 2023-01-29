a817ef4107036c20fd70ac2cf58f2360c2c86388
맥북 유찬홍 <chanhongy6@naver.com>
2023-01-29 23:36:52 +0900

    메인 화면

---
+++ counter_app/rx_dart_counter/lib/screen/my_home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('RxDart 카운터 앱'),
+      ),
+
+    );
+  }
+}
