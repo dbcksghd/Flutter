a8761325e536ceaf7f248f4d04ddb42949c4db34
맥북 유찬홍 <chanhongy6@naver.com>
2023-01-29 23:51:01 +0900

    뷰

--- counter_app/rx_dart_counter/lib/screen/my_home_page.dart
+++ counter_app/rx_dart_counter/lib/screen/my_home_page.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int count = 0;
+  int conut = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('RxDart 카운터 앱'),
+      appBar: AppBar(title: const Text('RxDart 카운터 앱')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '0',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
       ),
-
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () => {},
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
     );
   }
 }
