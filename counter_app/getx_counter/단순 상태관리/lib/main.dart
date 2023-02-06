ff12bd89a20b8ab1d0a586e714b835ebe2c29a40
desktop yoochanhong <chanhongy6@naver.com>
2023-02-07 05:29:40 +0900

    뷰에 GetBuilder 물려주기

--- counter_app/getx_counter/단순 상태관리/lib/main.dart
+++ counter_app/getx_counter/단순 상태관리/lib/main.dart
@@ -1,4 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:simple_state_management/controller/count_controller.dart';
 
 void main() => runApp(GetMaterialApp(home: MyApp()));
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+  final controller = Get.put(CountController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GetX 카운터 앱'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            GetBuilder<CountController>(
+              builder: (controller) => Text(controller.count.toString(),
+                  style: Theme.of(context).textTheme.headlineMedium),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
