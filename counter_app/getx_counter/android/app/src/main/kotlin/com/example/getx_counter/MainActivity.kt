4632d6e4447fdc372031f37c2067f00c959a3e1e
desktop yoochanhong <chanhongy6@naver.com>
2023-02-07 04:45:12 +0900

    다시 만들죠;

--- counter_app/getx_counter/android/app/src/main/kotlin/com/example/getx_counter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.getx_counter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- counter_app/getx_counter/lib/main.dart
+++
@@ -1,4 +0,0 @@
-import 'package:get/get.dart';
-import 'package:flutter/material.dart';
-
-void main() => runApp(GetMaterialApp(home: MyApp()));
