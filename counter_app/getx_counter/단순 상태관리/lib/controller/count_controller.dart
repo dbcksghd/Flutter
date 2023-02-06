fae663fe7bb131efe6d8c069cd0e75ec74bd2bf9
desktop yoochanhong <chanhongy6@naver.com>
2023-02-07 05:23:51 +0900

    카운터 함수가 들어있는 컨트롤러

---
+++ counter_app/getx_counter/단순 상태관리/lib/controller/count_controller.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+
+class CountController extends GetxController{
+  int count = 0;
+
+  void increment() {
+    count++;
+    update();
+  }
+
+  void decrement() {
+    count--;
+    update();
+  }
+}
