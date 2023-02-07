02b926bac9c086151fff125e0197e296d1e6795f
desktop yoochanhong <chanhongy6@naver.com>
2023-02-07 17:59:23 +0900

    뷰에 Obx 물려주기

--- counter_app/getx_counter/반응형 상태관리/lib/main.dart
+++ counter_app/getx_counter/반응형 상태관리/lib/main.dart
@@ -1,4 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:responsive_state_management/controller/count_controller.dart';
 
 void main() => runApp(GetMaterialApp(home: MyApp()));
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+  final controller = Get.put(CountController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GetX 카운터 앱'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Obx(
+              () => Text(
+                controller.count.toString(),
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
