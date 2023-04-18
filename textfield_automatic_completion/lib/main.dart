45ca0af19aff47645575b428e7f156a6097c75b0
yoochanhong <chanhongy6@naver.com>
2023-04-18 22:56:52 +0900

    TextField 생성

--- textfield_automatic_completion/lib/main.dart
+++ textfield_automatic_completion/lib/main.dart
@@ -24,6 +24,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(color: Colors.grey, child: TextField()),
+      ),
+    );
   }
 }
