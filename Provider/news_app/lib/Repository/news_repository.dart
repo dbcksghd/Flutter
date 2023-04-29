54902bdf8d1c8db9c67b568524001e6646a2219e
yoochanhong <chanhongy6@naver.com>
2023-04-29 20:29:54 +0900

    캡슐화

--- Provider/news_app/lib/Repository/news_repository.dart
+++ Provider/news_app/lib/Repository/news_repository.dart
@@ -1,8 +1,11 @@
 import 'package:news_app/DataSource/data_source.dart';
+import 'package:news_app/Model/articles.dart';
 import 'package:news_app/Model/news.dart';
 
 class NewsRepository {
   final DataSource _dataSource = DataSource();
 
   Future<News> getNews() => _dataSource.getNews();
+
+  Future<List<Articles>> getArticleList() => _dataSource.getArticlesList();
 }
