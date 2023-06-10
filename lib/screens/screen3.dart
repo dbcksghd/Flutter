d4cd5eeb820f6f52e22a4686177a4bf86d784769
yoochanhong <chanhongy6@naver.com>
2023-06-10 22:08:11 +0900

    그냥 페이지

---
+++ lib/screens/screen3.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Screen3 extends StatelessWidget {
+  const Screen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
